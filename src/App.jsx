d468ff3f5862bdc2f42fab35707c68c757acc8a7
ihomba92 <ihombakibandidavid@gmail.com>
2026-05-18 16:22:30 +0300

    initiated git

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import NavBar from './layout/NavBar'
+
+
+function App() {
+  return (
+    <>
+      <h1>React App</h1>
+    <NavBar />
+    </>
+  )
+}
+
+export default App
---
+++ src/layout/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+export default function NavBar() {    
+    return (
+       <nav> 
+        <ul>
+            <li><a href="home">Home</a></li>
+            <li><a href="about">About</a></li>
+        </ul>
+
+       </nav>
+    )
+}
